a006704ee5a5b4af30deb55887212d233eef3f20
Badi-Haki Martin <jojackblack@gmail.com>
2022-02-24 22:47:00 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity){
+    if(activity!=null){
+        return `This Saturday, I want to ${activity}!`;
+    }
+    else{
+        return `This Saturday, I want to roller-skate!`;
+    }
+}
+
+const mondayWork = function(activity){
+    if(activity!=null){
+        return `This Monday, I will ${activity}.`;
+    }
+    else{
+        return `This Monday, I will go to the office.`;
+    }
+}
+
+function wrapAdjective(flair = `*`){
+    return function(adjective=`special`){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
